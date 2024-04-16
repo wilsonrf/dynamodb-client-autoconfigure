19542d47d066429262b0b9af4a14410dfd50d19e
Wilson da Rocha Franca <wilsonrf@gmail.com>
2024-04-16 08:44:01 +0200

    code setup

---
+++ build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+    id("java")
+    id("org.springframework.boot") version "3.2.4"
+    id("io.spring.dependency-management") version "1.1.3"
+    id("com.github.ben-manes.versions") version "0.51.0"
+}
+
+group = "me.wilsonfranca"
+version = "1.0.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+val awsSdkVersion: String by project
+
+dependencies {
+    implementation(platform("software.amazon.awssdk:bom:$awsSdkVersion"))
+    implementation("org.springframework.boot:spring-boot-starter")
+    implementation("software.amazon.awssdk:dynamodb")
+    implementation("software.amazon.awssdk:dynamodb-enhanced")
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
