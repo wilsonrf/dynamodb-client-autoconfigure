372dacfde7e750f06b152ac5394133298c81fdb3
Wilson da Rocha Franca <wilsonrf@gmail.com>
2024-04-18 20:18:41 +0200

    add process to release a patch version

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,11 +14,7 @@
 * limitations under the License.
  */
 plugins {
-    id("java")
     id("java-library")
-    id("org.springframework.boot") version "3.2.4"
-    id("io.spring.dependency-management") version "1.1.3"
-    id("com.github.ben-manes.versions") version "0.51.0"
     id("org.springframework.boot") apply false
     id("io.spring.dependency-management")
     id("com.github.ben-manes.versions")
@@ -26,9 +22,10 @@ plugins {
 }
 
 group = "me.wilsonfranca"
-version = "1.0.0-SNAPSHOT"
+version = "1.0.0"
 
 repositories {
+    mavenLocal()
     mavenCentral()
 }
 
@@ -56,4 +53,114 @@ dependencies {
 
 tasks.test {
     useJUnitPlatform()
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            versionMapping {
+                usage("java-api") {
+                    fromResolutionOf("runtimeClasspath")
+                }
+                usage("java-runtime") {
+                    fromResolutionResult()
+                }
+            }
+        }
+    }
+}
+
+/**
+ * This task will create a new patch release from the current snapshot version.
+ * It will update the version in the files the version is present.
+ * The release version is the current version without the -SNAPSHOT suffix taken from the main branch.
+ * The task will create a tag in the git repository with the release version.
+ * Next, it will push the tag to the remote repository.
+ * Then it will create a new patch snapshot version in the main branch
+ * updating the version in the files the version is present, commit
+ * and push the changes to the remote repository.
+ *
+ */
+tasks.register("createNewPatchReleaseFromSnapshot") {
+    group = "publishing"
+    doLast {
+        val version = currentVersion()
+        val releaseVersion = version.replace("-SNAPSHOT", "")
+        updateVersionInFiles(releaseVersion)
+        createTagBeforeRelease(releaseVersion)
+        val nextSnapshotVersion =
+            "${releaseVersion.split(".")[0]}.${releaseVersion.split(".")[1]}.${releaseVersion.split(".")[2].toInt() + 1}"
+        createSnapshotAfterRelease(nextSnapshotVersion)
+    }
+}
+
+fun currentVersion(): String {
+    return version.toString()
+}
+
+fun currentMajor(): Int {
+    return currentVersion().split(".")[0].toInt()
+}
+
+fun currentMinor(): Int {
+    return version.toString().split(".")[1].toInt()
+}
+
+fun currentPatch(): Int {
+    return version.toString().split(".")[2].toInt()
+}
+
+fun filesToBeUpdated(): List<String> {
+    return listOf("build.gradle.kts")
+}
+
+fun updateVersionInFiles(version: String) {
+    filesToBeUpdated().forEach {
+        val file = File(it)
+        val content = file.readText()
+        val updatedContent = content.replace(currentVersion(), version)
+        file.writeText(updatedContent)
+    }
+}
+
+fun createTagBeforeRelease(version: String, push: Boolean = false) {
+    val process = ProcessBuilder("git", "tag", "-a", version, "-m", "Release $version").start()
+    process.waitFor(10, TimeUnit.SECONDS)
+    val exitCode = process.exitValue()
+    if (exitCode != 0) {
+        throw RuntimeException("Failed to create tag")
+    }
+    if (push) {
+        val process1 = ProcessBuilder("git", "push", "origin", version).start()
+        process1.waitFor(10, TimeUnit.SECONDS)
+        val exitCode1 = process.exitValue()
+        if (exitCode1 != 0) {
+            throw RuntimeException("Failed to push tag")
+        }
+    }
+}
+
+fun createSnapshotAfterRelease(version: String, push: Boolean = false) {
+    val nextSnapshotVersion = "${version.split(".")[0]}.${version.split(".")[1]}.${version.split(".")[2]}-SNAPSHOT"
+    updateVersionInFiles(nextSnapshotVersion)
+    val process2 = ProcessBuilder("git", "add", ".").start()
+    process2.waitFor(10, TimeUnit.SECONDS)
+    val exitCode2 = process2.exitValue()
+    if (exitCode2 != 0) {
+        throw RuntimeException("Failed to add files")
+    }
+    val process3 = ProcessBuilder("git", "commit", "-m", "New snapshot version $nextSnapshotVersion").start()
+    process3.waitFor(10, TimeUnit.SECONDS)
+    val exitCode3 = process3.exitValue()
+    if (exitCode3 != 0) {
+        throw RuntimeException("Failed to commit changes")
+    }
+    if (push) {
+        val process4 = ProcessBuilder("git", "push", "origin", "main").start()
+        process4.waitFor(10, TimeUnit.SECONDS)
+        val exitCode4 = process4.exitValue()
+        if (exitCode4 != 0) {
+            throw RuntimeException("Failed to push changes")
+        }
+    }
 }
